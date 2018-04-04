245cacd74b473b06c030ab3590ed8d0b20eafc97
Levi Rosenberg <levirosenberg97@gmail.com>
2018-04-04 12:03:42 -0700

    added turret functionality

---
+++ Assets/Scripts/TankHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankHealth : MonoBehaviour
+{
+    public int health;
+    public bool isDead = false;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+
+    void Death()
+    {
+        isDead = true;
+        gameObject.SetActive(false);
+        
+    }
+
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            Death();
+        }
+    }
+}
--- Assets/Scripts/TurretManager.cs
+++ Assets/Scripts/TurretManager.cs
@@ -4,58 +4,155 @@ using UnityEngine;
 
 public class TurretManager : MonoBehaviour
 {
-    public string tagName;
+    public List<Transform> targets;
+    public string objectTag;
+    private Transform selectedObject;
+    private Transform MyTransform;
+
+    public int damage;
+    public float timeBetweenShots;
+    Ray shootRay;
+    RaycastHit shootHit;
+    float timer;
+    float range = 100f;
+    int shootableMask;
+
+    //ParticleSystem gunParticles;
+    LineRenderer gunLine;
+    Light gunLight;
+    public float effectsTime;
 
 
-    public GameObject FindClosestEnemy()
+    private void Start()
     {
-        GameObject[] gos;
+        shootableMask = LayerMask.GetMask("Shootable");
 
-        gos = GameObject.FindGameObjectsWithTag(tagName);
-        GameObject closest = null;
-        float distance = Mathf.Infinity;
-        Vector3 position = transform.position;
-        foreach (GameObject go in gos)
-        {
-            
+        targets = new List<Transform>();
+        MyTransform = transform;
+        //gunParticles = GetComponent<ParticleSystem>();
+        gunLine = GetComponent<LineRenderer>();
+        gunLight = GetComponent<Light>();
 
-            Vector3 diff = go.transform.position - position;
-            float curDistance = diff.sqrMagnitude;
-            if (curDistance < distance)
-            {
-                closest = go;
-                distance = curDistance;
-            }
-        }
-        return closest;
+        timer = 0f;
     }
 
-    // Use this for initialization
-
-   
-
-    public GameObject[] gos;
-
-    public int maxRange;
-    public int minRange;
-    private Vector3 targetTran;
-
-
-	void Start ()
+    void Shoot()
     {
-   
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        GameObject target = FindClosestEnemy();
+        timer = 0f;
 
-		if ((Vector3.Distance(transform.position,target.transform.position) < maxRange)&& Vector3.Distance(transform.position, target.transform.position) > minRange)
+        gunLight.enabled = true;
+        gunLine.enabled = true;
+        gunLine.SetPosition(0, transform.position);
+        //gunParticles.Play();
+        shootRay.origin = transform.position;
+        shootRay.direction = transform.forward;
+
+        if(Physics.Raycast(shootRay,out shootHit,range, shootableMask))
         {
-            transform.LookAt(targetTran);
+            TankHealth tankHealth = shootHit.collider.GetComponent<TankHealth>();
 
-            transform.Translate(Vector3.forward * Time.deltaTime);
+            if (tankHealth != null)
+            {
+                tankHealth.TakeDamage(damage);
+            }
+
+            
+
+            gunLine.SetPosition(1, shootHit.point);
         }
-	}
+        else
+        {
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+    }
+
+    void DisableEffects()
+    {
+        gunLine.enabled = false;
+        gunLight.enabled = false;
+        //gunParticles.Stop();
+    }
+
+    //public void AddAllObjects()
+    //{
+    //    GameObject[] gos = GameObject.FindGameObjectsWithTag(objectTag);
+
+    //    foreach(GameObject thing in gos)
+    //    {
+    //        if (!targets.Contains(thing.transform))
+    //        {
+    //            AddTarget(thing.transform);
+    //        }
+    //    }
+    //}
+
+    public void AddTarget(Transform thing)
+    {
+        targets.Add(thing);
+    }
+
+    private void SortTargetsByDistance()
+    {
+        targets.Sort(delegate (Transform t1, Transform t2)
+        {
+            return Vector3.Distance(t1.position, MyTransform.position).CompareTo
+            (Vector3.Distance(t2.position, MyTransform.position));
+        });
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (targets.Count > 0)
+        {
+            if (targets[0] != null)
+            {
+                SortTargetsByDistance();
+                selectedObject = targets[0];
+
+                for (int i = 0; i < targets.Count; i++)
+                {
+                    if (targets[i].gameObject.activeInHierarchy == false)
+                    {
+                        targets.Remove(targets[i]);
+                    }
+                }
+
+                if (selectedObject != null && timer >= timeBetweenShots)
+                {
+                    Shoot();
+                }
+
+                if (timer >= timeBetweenShots * effectsTime)
+                {
+                    DisableEffects();
+                }
+
+                transform.LookAt(selectedObject);
+            }
+
+        }
+    }
+            
+
+        
+    
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == objectTag)
+        {
+            targets.Add(other.gameObject.transform);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == objectTag)
+        {
+            targets.Remove(other.gameObject.transform);
+        }
+    }
+
 }
